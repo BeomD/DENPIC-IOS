12eb7d6b1d302e8dc64e36e98489b8944678c5fe
Beomjin Kim <bamjin204@gmail.com>
2022-12-18 09:53:27 +0900

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by BJ Man on 2022/12/18.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
